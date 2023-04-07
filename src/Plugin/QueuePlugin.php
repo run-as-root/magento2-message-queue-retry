d196b066cafdd5dcd1b08c1763e49eb4ddd86178
Vladyslav Sikailo <vladyslav.sikailo@run-as-root.sh>
2023-04-07 18:24:03 +0300

    - Replace Rewrite by Around Plugin on message reject function - Fix Retries Count - Refactoring - Update Tests

---
+++ src/Plugin/QueuePlugin.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types = 1);
+
+namespace RunAsRoot\MessageQueueRetry\Plugin;
+
+use JsonException;
+use Magento\Framework\MessageQueue\EnvelopeInterface;
+use Magento\Framework\MessageQueue\QueueInterface;
+use RunAsRoot\MessageQueueRetry\Exception\MessageCouldNotBeCreatedException;
+use RunAsRoot\MessageQueueRetry\Service\IsMessageShouldBeSavedForRetryService;
+use RunAsRoot\MessageQueueRetry\Service\SaveFailedMessageService;
+
+class QueuePlugin
+{
+    public function __construct(
+        private IsMessageShouldBeSavedForRetryService $isMessageShouldBeSavedForRetryService,
+        private SaveFailedMessageService $saveFailedMessageService
+    ) {
+    }
+
+    /**
+     * @throws MessageCouldNotBeCreatedException
+     * @throws JsonException
+     */
+    public function aroundReject(
+        QueueInterface $subject,
+        callable $proceed,
+        EnvelopeInterface $envelope,
+        bool $requeue,
+        string $error
+    ): void {
+        if (!$error) {
+            $proceed($envelope, $requeue, $error);
+        }
+
+        $shouldBeSavedForRetry = $this->isMessageShouldBeSavedForRetryService->execute($envelope);
+
+        if (!$shouldBeSavedForRetry) {
+            $proceed($envelope, $requeue, $error);
+            return;
+        }
+
+        $this->saveFailedMessageService->execute($envelope, $error);
+        $subject->acknowledge($envelope);
+    }
+}
---
+++ src/Service/IsMessageShouldBeSavedForRetryService.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RunAsRoot\MessageQueueRetry\Service;
+
+use JsonException;
+use Magento\Framework\MessageQueue\EnvelopeInterface;
+use RunAsRoot\MessageQueueRetry\System\Config\MessageQueueRetryConfig;
+
+class IsMessageShouldBeSavedForRetryService
+{
+    public function __construct(
+        private MessageQueueRetryConfig $messageQueueRetryConfig,
+        private GetMessageRetriesCountService $getMessageRetriesCountService
+    ) {
+    }
+
+    /**
+     * @throws JsonException
+     */
+    public function execute(EnvelopeInterface $message): bool
+    {
+        if (!$this->messageQueueRetryConfig->isDelayQueueEnabled()) {
+            return false;
+        }
+
+        $totalRetries = $this->getMessageRetriesCountService->execute($message);
+        if ($totalRetries === 0) {
+            return false;
+        }
+
+        $messageProperties = $message->getProperties();
+        $topicName = $messageProperties['topic_name'] ?? null;
+        if (!$topicName) {
+            return false;
+        }
+
+        $queueConfiguration = $this->getQueueConfiguration($topicName);
+        if (!$queueConfiguration) {
+            return false;
+        }
+
+        $retryLimit = $queueConfiguration[MessageQueueRetryConfig::RETRY_LIMIT] ?? 0;
+
+        if ($totalRetries >= $retryLimit) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @throws JsonException
+     * @return array<string,mixed>|null
+     */
+    private function getQueueConfiguration(string $topicName): ?array
+    {
+        $delayQueueConfiguration = $this->messageQueueRetryConfig->getDelayQueues();
+        return $delayQueueConfiguration[$topicName] ?? null;
+    }
+}
